b76a3379d085bf73208f4dc50514060d209f77e1
Lilly Daniell <daniell.lilly@gmail.com>
2021-08-19 09:18:40 +0100

    Define method for user inputting student names

--- directory.rb
+++ directory.rb
@@ -27,6 +27,23 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)

152f850eba2239f5ec9ba780ab63fcaff5805b08
Lilly Daniell <daniell.lilly@gmail.com>
2021-08-20 10:25:48 +0100

    Web application loads previous student data from the command line

--- directory.rb
+++ directory.rb
@@ -1,23 +1,5 @@
 @students = [] # an empty array saved to an instance variable, accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = gets.chomp
-  while !name.empty? do # while the name is not empty, repeat this code
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} student(s)"
-    name = gets.chomp # get another name from the user
-  end
-end
-
-def interactive_menu
-  loop do 
-    print_menu # print the menu and ask the user what to do
-    process(gets.chomp) # read the input and save it to a variable / do what the user has asked
-  end
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -26,20 +8,33 @@ def print_menu
   puts "9. Exit"
 end
 
+def interactive_menu
+  loop do 
+    print_menu # print the menu and ask the user what to do
+    process(STDIN.gets.chomp) # read the input and save it to a variable / do what the user has asked
+  end
+end
+
 def process(selection)
   case selection
-  when "1" # input the students
-    input_students 
-  when "2" # show the students
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9" 
-    exit # this will cause the program to terminate
+  when "1" then input_students 
+  when "2" then show_students
+  when "3" then save_students
+  when "4" then load_students
+  when "9" then exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do # while the name is not empty, repeat this code
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} student(s)"
+    name = STDIN.gets.chomp # get another name from the user
   end
 end
 
@@ -49,40 +44,52 @@ def show_students
   print_footer
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w") # w = write in file
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",") # csv = comma-separated variable
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r") # r = read only
-  file.readlines.each do |line| 
-    name, cohort = line.chomp.split(",") # parallel assignment of an array (from .split method on a string)
-      @students << {name: name, cohort: cohort.to_sym} # from string .to_sym for consistency
-  end
-  file.close
-end
-
 def print_header
   puts "The students of Makers Academy"
   puts "-------------"
 end
 
 def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
-  end
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)" }
 end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w") # w = write in file
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]] 
+    csv_line = student_data.join(",") # csv = comma-separated variable
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename= "students.csv") # method now reads the filename as an argument with a default value
+  file = File.open(filename, "r") # r = read only
+  file.readlines.each do |line| 
+    name, cohort = line.chomp.split(",") # parallel assignment of an array (from .split method on a string)
+    # chomp here discards the trailing new line character from the line
+      @students << {name: name, cohort: cohort.to_sym} # cohort from string .to_sym for consistency
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if file does exist
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if file doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quits the program
+  end
+end
+
+try_load_students
 interactive_menu

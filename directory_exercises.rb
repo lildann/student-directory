f7a64b81b39b07bd6b38a967aed918e69e6242c8
Lilly Daniell <daniell.lilly@gmail.com>
2021-08-20 12:37:16 +0100

    Add Student Directory exercises

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -62,25 +62,23 @@ def save_students
   if filename.empty?
     filename = "students.csv"
   end
-  file = File.open(filename, "w") # open the file for writing (w = write in file)
+  file = File.open(filename, "w") { |file|
   @students.each do |student|
     student_data = [student[:name], student[:cohort]] 
     csv_line = student_data.join(",") 
     file.puts csv_line
-  end
-  file.close
+  end }
   puts "Students saved to file"
 end
 
 def load_students(filename = "students.csv") # method now reads the filename as an argument with a default value
   puts "What file should this be loaded from?"
   filename = STDIN.gets.chomp
-  file = File.open(filename, "r") # r = read only
-  file.readlines.each do |line| 
-    name, cohort = line.chomp.split(",") 
-    collect_students_in_array(name, cohort)
-  end
-  file.close
+  file = File.open(filename, "r") { |file| 
+    file.readlines.each do |line| 
+      name, cohort = line.chomp.split(",") 
+      collect_students_in_array(name, cohort)
+    end }
   puts "Students loaded to file"
 end
 
